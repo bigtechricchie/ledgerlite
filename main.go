8aec5498d8137d4dd69ae0924ef7e636dc1a1f3f
edward siwek <274587671+bigtechricchie@users.noreply.github.com>
2026-08-19 18:20:24 +0100

    feat: add transaction model

--- main.go
+++ main.go
@@ -2,7 +2,32 @@ package main
 
 import "fmt"
 
+type Transaction struct {
+	ID          string
+	Account     string
+	Type        string
+	Amount      float64
+	Description string
+}
+
 func main() {
+	transaction := Transaction{
+		ID:          "TXN-001",
+		Account:     "FUND-001",
+		Type:        "DEPOSIT",
+		Amount:      10000,
+		Description: "Initial funding",
+	}
+
 	fmt.Println("LedgerLite")
 	fmt.Println("Simple Banking Ledger")
+	fmt.Println()
+
+	fmt.Println("Transaction")
+	fmt.Println("-----------")
+	fmt.Println("ID:", transaction.ID)
+	fmt.Println("Account:", transaction.Account)
+	fmt.Println("Type:", transaction.Type)
+	fmt.Println("Amount:", transaction.Amount)
+	fmt.Println("Description:", transaction.Description)
 }
